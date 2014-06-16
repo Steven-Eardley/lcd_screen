70cfec51b31e3201a1c86d9762b34967571d7f72
nimro <me@nimro.org>
2014-06-17 00:01:51 +0100

    moved wiring comments from screenController to doc/wiring

--- screenController.py
+++ screenController.py
@@ -1,41 +1,9 @@
-#!/usr/bin/python
+# A class to control the LCD Keypad Shield's screen.
 #
-# HD44780 LCD Test Script for
-# Raspberry Pi
+# Authors: Steven Eardley and Duncan Blair
+# Copyright 2014
 #
-# Author : Matt Hawkins
-# Site   : http://www.raspberrypi-spy.co.uk
-#
-# Date   : 26/07/2012
-#
- 
-# The wiring for the LCD is as follows:
-# 1 : GND
-# 2 : 5V
-# 3 : Contrast (0-5V)*
-# 4 : RS (Register Select)
-# 5 : R/W (Read Write)       - GROUND THIS PIN
-# 6 : Enable or Strobe
-# 7 : Data Bit 0             - NOT USED
-# 8 : Data Bit 1             - NOT USED
-# 9 : Data Bit 2             - NOT USED
-# 10: Data Bit 3             - NOT USED
-# 11: Data Bit 4
-# 12: Data Bit 5
-# 13: Data Bit 6
-# 14: Data Bit 7
-# 15: LCD Backlight +5V**
-# 16: LCD Backlight GND
- 
-
-# Basic write commands for later:
-#lcd_byte(LCD_LINE_1, LCD_CMD)
-#lcd_string("hello, world.")
-#lcd_byte(LCD_LINE_2, LCD_CMD)
-#lcd_string("IT WORKS!")
-
-# Quick sleep command:
-#time.sleep(20)
+# __init__ section based on HD44780 Test Script by Matt Hawkins.
 
 #import
 import RPi.GPIO as GPIO
