8c2145594b253fbfc3283b7e80ce2affcf8bc6b1
nimro <me@nimro.org>
2014-06-17 00:16:37 +0100

    added some comments, formatting

--- screenController.py
+++ screenController.py
@@ -20,9 +20,9 @@ class screenController:
         self.LCD_D7 = 17
          
         # Define some device constants
-        self.LCD_WIDTH = 16    # Maximum characters per line
-        self.LCD_CHR = True
-        self.LCD_CMD = False
+        self.LCD_WIDTH  = 16   # Maximum characters per line
+        self.LCD_CHR    = True
+        self.LCD_CMD    = False
         self.LCD_LINE_1 = 0x80 # LCD RAM address for the 1st line
         self.LCD_LINE_2 = 0xC0 # LCD RAM address for the 2nd line 
          
@@ -30,7 +30,12 @@ class screenController:
         self.E_PULSE = 0.00005
         self.E_DELAY = 0.00005
 
-        GPIO.setmode(GPIO.BCM)       # Use BCM GPIO numbers
+        # Suppress terminal warnings that pins are in use.
+        # This allows rewriting messages without being nagged.
+        GPIO.setwarnings(False)
+
+        # Some GPIO set-up 
+        GPIO.setmode(GPIO.BCM)            # Use BCM GPIO numbers
         GPIO.setup(self.LCD_E, GPIO.OUT)  # E
         GPIO.setup(self.LCD_RS, GPIO.OUT) # RS
         GPIO.setup(self.LCD_D4, GPIO.OUT) # DB4
@@ -40,24 +45,6 @@ class screenController:
      
         # Initialise display
         self.lcd_init()
-     
-
-    def lcd_init(self):
-        # Initialise display
-        self.lcd_byte(0x33,self.LCD_CMD)
-        self.lcd_byte(0x32,self.LCD_CMD)
-        self.lcd_byte(0x28,self.LCD_CMD)
-        self.lcd_byte(0x0C,self.LCD_CMD)
-        self.lcd_byte(0x06,self.LCD_CMD)
-        self.lcd_byte(0x01,self.LCD_CMD)  
-
-    def lcd_string(self, message):
-        # Send string to display
-
-        message = message.ljust(self.LCD_WIDTH," ")  
-
-        for i in range(self.LCD_WIDTH):
-            self.lcd_byte(ord(message[i]),self.LCD_CHR)
 
     def lcd_byte(self, bits, mode):
         # Send byte to data pins
@@ -109,11 +96,30 @@ class screenController:
         GPIO.output(self.LCD_E, False)
         time.sleep(self.E_DELAY)   
 
+    def lcd_init(self):
+        # Initialise display
+        self.lcd_byte(0x33,self.LCD_CMD)
+        self.lcd_byte(0x32,self.LCD_CMD)
+        self.lcd_byte(0x28,self.LCD_CMD)
+        self.lcd_byte(0x0C,self.LCD_CMD)
+        self.lcd_byte(0x06,self.LCD_CMD)
+        self.lcd_byte(0x01,self.LCD_CMD)  
+
+    def lcd_string(self, message):
+        # Send string to display
+
+        message = message.ljust(self.LCD_WIDTH," ")  
+
+        for i in range(self.LCD_WIDTH):
+            self.lcd_byte(ord(message[i]),self.LCD_CHR)
+
     def println1(self, input):
+    # Prints input to the top line of the LCD
         self.lcd_byte(self.LCD_LINE_1, self.LCD_CMD)
         self.lcd_string(input)
         
     def println2(self, input):
+    # Prints input to the bottom line of the LCD
         self.lcd_byte(self.LCD_LINE_2, self.LCD_CMD)
         self.lcd_string(input)
 
