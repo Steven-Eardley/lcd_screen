5a81ce546d49b343b8d1af93563b21ca753ce881
Steven Eardley <me@steveneardley.co.uk>
2014-06-17 20:22:39 +0000

    implemented requested functionality in scrollBuffer

--- scrollBuffer.py
+++ scrollBuffer.py
@@ -4,22 +4,30 @@
 # Copyright 2014
 
 class scrollBuffer:
-    def __init__(self):
+    def __init__(self, init_text=None):
         # Create the buffer list
-        self.buffer = ["", ""]
+        self.buffer = []
+        
+        if init_text:
+            self.write(init_text)
 
     def write(self, line_text):
-    # writes to the end of the buffer
-    #TODO: this should break the string into 16-char chunks
-        self.buffer.append(line_text)
+    # writes to the end of the buffer, in 16 char chunks
+    # we use strip() to remove trailing spaces across lines
+        chunks = [ line_text[i:i+16].strip() for i in range(0, len(line_text), 16) ]
+        self.buffer += chunks
 
     def read_line(self, line_num):
     # reads a specific line from the buffer
-        return self.buffer[line_num]
+        try:
+            return self.buffer[line_num]
+        except:
+            return None
 
-    def length(self)
+    def length(self):
     # returns the length of the buffer
         return len(self.buffer)
 
-    def read(self)
-    # this should return the last two lines
+    def read(self, n_lines=2):
+    # return the final two lines of the buffer
+        return self.buffer[self.length() - n_lines:]
