5f1e53d466875274c56eb0a92f61903d6e7e9326
Steven Eardley <me@steveneardley.co.uk>
2014-08-02 23:58:24 +0100

    added clean scrollbuffer write and methods to clear, read all.

--- scrollBuffer.py
+++ scrollBuffer.py
@@ -1,7 +1,6 @@
 # A class to hold scrollback buffers
 #
 # Authors: Steven Eardley and Duncan Blair
-# Copyright 2014
 
 class scrollBuffer:
     def __init__(self, init_text=None):
@@ -17,13 +16,29 @@ class scrollBuffer:
         if clean_breaks:
             self.write_clean(line_text)
         else:
-            chunks = [ line_text[i:i+16].strip() for i in range(0, len(line_text), 16) ]
-            self.buffer += chunks
+            self.write_simple(line_text)
             
     def write_clean(self, line_text):
     # breaks lines cleanly on word boundaries
-        chunks = []
-        self.buffer += chunks
+        words = line_text.split()
+	
+        # first word is first chunk(s)
+	w0 = words[0]
+	chunks = [ w0[i:i+16] for i in range(0, len(w0), 16) ]
+    
+        # if the word fits in the last chunk, add there, else, form new chunk
+	for w in words[1:]:
+	    if len(chunks[-1]) + len(w) < 15:
+	        chunks[-1] += " " + w
+            else:
+                chunks += [ w[i:i+16] for i in range(0, len(w), 16) ]
+	
+	self.buffer += chunks
+
+    def write_simple(self, line_text):
+    # writes to buffer, 16 chars at a time
+	chunks = [ line_text[i:i+16].strip() for i in range(0, len(line_text), 16) ]
+	self.buffer += chunks
 
     def read_line(self, line_num):
     # reads a specific line from the buffer
@@ -39,3 +54,10 @@ class scrollBuffer:
     def read(self, n_lines=2):
     # return the final two lines of the buffer
         return self.buffer[self.length() - n_lines:]
+
+    def read_all(self):
+	return self.read(len(self.buffer))
+
+    def clear(self):
+	self.buffer = []
+
